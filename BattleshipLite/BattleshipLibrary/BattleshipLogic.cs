cb6157bf928003066c87911a94110b1680367b1f
shajib740 <shajib740@gmail.com>
2023-04-18 00:41:26 -0400

    User Interface Design Completed

---
+++ BattleshipLite/BattleshipLibrary/BattleshipLogic.cs
@@ -0,0 +1,118 @@
+﻿using BattleshipLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLibrary
+{
+    public class BattleshipLogic
+    {
+        public static void InitializeShotGrid(PlayerModel model)
+        {
+            List<string> letters = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            foreach (string letter in letters)
+            {
+                foreach (int number in numbers)
+                {
+                    AddGridSpot(model, letter, number);
+                }
+
+            }
+        }
+
+        public static bool PlaceShip(PlayerModel model, string location)
+        {
+            string letter = location.Substring(0,1);
+            int number ;
+            
+            if ( !int.TryParse(location.Substring(1,1), out number) ||  string.IsNullOrEmpty(letter) )
+            {
+                return false;
+            }
+            GridSpotModel gridSpot = new GridSpotModel
+            {
+                SpotLetter = letter,
+                SpotNumber = number,
+                Status = GridSpotStatus.Empty
+            };
+
+            if (!model.ShotGrid.Contains(gridSpot))
+            {
+                return false;
+            }
+           else
+            {
+                return true;
+            }
+        }
+
+        private static void AddGridSpot(PlayerModel model, string letter, int number)
+        {
+            GridSpotModel gridSpot = new GridSpotModel
+            {
+                SpotLetter = letter,
+                SpotNumber = number,
+                Status = GridSpotStatus.Empty
+            };
+
+            model.ShotGrid.Add(gridSpot);
+        }
+        public static void AddShipPlcae(PlayerModel model, string location, GridSpotStatus status)
+        {
+           ( string letter, int number ) = SplitShotByRowAndColumn(location);
+           
+         
+            GridSpotModel grid = new GridSpotModel();
+            grid.SpotLetter = letter;
+            grid.SpotNumber = number;
+            grid.Status = GridSpotStatus.Ship;
+            model.ShipLocation.Add(grid);
+        }
+
+        public static bool IsGameOver(PlayerModel opponent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetShotCount(PlayerModel winner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static (string row, int column) SplitShotByRowAndColumn(string shot)
+        {
+            string row = shot.Substring(0, 1);
+            int column = int.Parse(shot.Substring(1, 1));
+
+            return (row, column);
+        }
+
+        public static bool ValidateShot(PlayerModel activePlayer, string row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IdentifyShotResult(PlayerModel opponent, string row, int column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void MarkShotResult(PlayerModel opponent, string row, int column, bool isAHit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BattleshipLite/GameUI/Program.cs
+++ BattleshipLite/GameUI/Program.cs
@@ -1,9 +1,11 @@
 ﻿using BattleshipLibrary.Models;
+using BattleshipLibrary;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace GameUI
 {
@@ -27,22 +29,105 @@ namespace GameUI
                 Console.WriteLine("Please fire a shot");
                 string shotFired = Console.ReadLine();
                 // Determine if it is a valid shot
-                // bool isValidShot = isValidShot()
                 // Determine the result
-                // Determine  if the game is over
-                // else, go to opponent (swap position)
 
+                RecordPlayerShot(activePlayer, opponent);
+
+
+                // Determine  if the game should continue
+
+                bool isGameOver = BattleshipLogic.IsGameOver(opponent);
+
+                if (isGameOver == true)
+                {
+                    winner = activePlayer;
+                }
+                else
+                {
+                    // Swap using a temp variable
+                    //PlayerModel tempPlayer = opponent;
+                    //opponent = activePlayer;
+                    //activePlayer = tempPlayer;
+
+                    // Swap using tuple
+                    (activePlayer, opponent) = (opponent, activePlayer);
+                }
 
             } while (winner == null);
+
+            IdentifyWinner(winner);
 
             Console.ReadLine();
         }
 
+        private static void IdentifyWinner(PlayerModel winner)
+        {
+            Console.WriteLine($"Congratulations to {winner.UserName}");
+            Console.WriteLine($"{winner.UserName} tool {BattleshipLogic.GetShotCount(winner)} shots.");
+        }
+
+        private static void RecordPlayerShot(PlayerModel activePlayer, PlayerModel opponent)
+        {
+            bool isValidShot = false;
+            string row;
+            int column;
+            do
+            {
+                string shot = AskforShot();
+                ( row,  column) = BattleshipLogic.SplitShotByRowAndColumn(shot);
+
+                isValidShot = BattleshipLogic.ValidateShot(activePlayer, row, column);
+                if (isValidShot == false)
+                {
+                    Console.WriteLine("Invalid shot location. Please try again");
+                }
+
+            } while (isValidShot == false);
+
+
+            // Determine shot results (hit/miss)
+            bool isAHit = BattleshipLogic.IdentifyShotResult(opponent, row, column);
+
+            // Record results
+            BattleshipLogic.MarkShotResult(opponent, row, column, isAHit);
+        }
+
+        private static string AskforShot()
+        {
+            Console.WriteLine("Please enter your shot selection");
+            string output = Console.ReadLine();
+            return output;  
+        }
+
         private static void DisplayshotGrid(PlayerModel activePlayer)
         {
+            string currentRow = activePlayer.ShotGrid[0].SpotLetter;
+
             foreach (var gridspot in activePlayer.ShotGrid)
             {
-                Console.Write($"Shot location {gridspot.SpotLetter}/ {gridspot.SpotNumber} / {gridspot.Status} ");
+                if (currentRow != gridspot.SpotLetter)
+                {
+                    currentRow = gridspot.SpotLetter;
+                    Console.WriteLine();
+                }
+                if (gridspot.Status != GridSpotStatus.Empty)
+                {
+                    Console.Write($"Shot location {gridspot.SpotLetter}{gridspot.SpotNumber} ");
+                }
+                else if (gridspot.Status == GridSpotStatus.Hit)
+                {
+                    Console.WriteLine(" X ");
+                }
+                else if (gridspot.Status == GridSpotStatus.Miss)
+                {
+                    Console.WriteLine(" O ");
+                }
+                else
+                {
+                    Console.Write(" ? ");
+                }
+
+
             }
         }
 
@@ -60,7 +145,7 @@ namespace GameUI
             // ask player for their name
             output.UserName = AskForPlayerName();
             // Load up the shot grid
-          //  BattleshipLogic.InitializeShotGrid(output);
+            BattleshipLogic.InitializeShotGrid(output);
 
             // Ask the user for their 5 ship placements
             PlaceShip(output);
@@ -78,7 +163,22 @@ namespace GameUI
         }
         private static void PlaceShip(PlayerModel model)
         {
-           
+            do
+            {
+                Console.Write($"Where do you want to place ship number {model.ShipLocation.Count + 1}:");
+                string location = Console.ReadLine();
+
+                bool isValidLocation = BattleshipLogic.PlaceShip(model, location);
+                if (isValidLocation == false)
+                {
+                    Console.WriteLine("That was not a valid location. Please add again.");
+                }
+                else
+                {
+                    BattleshipLogic.AddShipPlcae(model, location, GridSpotStatus.Ship);
+                }
+
+            } while (model.ShipLocation.Count < 5);
         }
     }
 }
